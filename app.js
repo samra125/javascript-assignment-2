9e1f038f0baaf91584c1d73aee551721669bef88
samraansari <samraansari27@gmail.com>
2020-06-23 23:02:56 +0500

    chapter 21 to 38 javascript assignment assignment

---
+++ app.js
@@ -0,0 +1,39 @@
+//  .....................chapter 21 to 25/////
+var cleanestCities = ["Cheyenne", "cheyenne", "Santa Fe", "santa fe", "Tucson", tucson", "Great Falls",
+"great falls", "Honolulu", "honolulu"];
+
+
+
+ var cityToCheck = prompt("Enter your city");
+ cityToCheck = cityToCheck.toLowerCase();
+ var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+ for (var i = 0; i <= 4; i++) {
+ if (cityToCheck === cleanestCities[i]) {
+ alert("It's one of the cleanest cities");
+ }
+
+
+
+  for (var i = 0; i < text.length; i++) {
+     if (text.slice(i, i + 12) === "World War II") {
+     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+     }
+     }
+
+
+ for (var i = 0; i < text.length; i++) {
+     if (text.slice(i, i + 12) === "World War II") {
+     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+     }
+    }
+//......................chapter26 to  30////
+
+
+var bigDecimal = Math.random();
+ var improvedNum = (bigDecimal * 6) + 1;
+ var numberOfStars = Math.floor(improvedNum);
+//..................chapter 31 to 34////
+
+
+
+//,,,,,,,,,,,,,,,,,,,,,,,,,chapter 35 to 38/////////////
